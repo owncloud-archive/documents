10e32dfc90c6b9b7802ce02e49d353241a665a0c
Vincent Petry <PVince81@yahoo.fr>
2013-10-18 15:41:21 +0200

    Added file actions
    Provides "View" action for when clicking on a document name in the files
    list (#71)

---
+++ js/fileactions.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+	"use strict";
+	
+	// add file action to file list, if available
+	if(typeof FileActions!=='undefined'){
+		var supportedMimes = [
+			'application/vnd.oasis.opendocument.text'
+		];
+		for (var i = 0; i < supportedMimes.length; ++i){
+			var mime = supportedMimes[i];
+			FileActions.register(mime,'View',OC.PERMISSION_READ,'',function(filename){
+				var dir = $('#dir').val().replace(/%2F/g, '/');
+				window.location = OC.linkTo('documents', 'index.php') + '?file=' + encodeURIComponent(dir + '/' + filename);
+			});
+			FileActions.setDefault(mime,'View');
+		}
+	}
+});
